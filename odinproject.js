30bd54487fcf30e45c6c849b46887453d3a0e496
dottereldesign <howemanning@gmail.com>
2022-03-15 01:21:49 +1300

    Progress through TOP JS Foundations 2.

--- odinproject.js
+++ odinproject.js
@@ -1,3 +1,11 @@
-let huh = 1 === '1';
+let huh = 1 == 1;
 
 console.log(huh);
+
+const string = 'the revolution will not be televised.';
+console.log(string);
+const badString = string;
+console.log(badString);
+
+let greeting = `hello, ${string}`;
+console.log(greeting);
