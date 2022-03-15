5b061636b7c71a55bd3adc4984d72fe9677983fd
dottereldesign <howemanning@gmail.com>
2022-03-15 19:01:28 +1300

    Progress to Fundamentals Part 3 - Functions.

--- odinproject.js
+++ odinproject.js
@@ -1,11 +1,44 @@
-let huh = 1 == 1;
-
-console.log(huh);
-
-const string = 'the revolution will not be televised.';
-console.log(string);
-const badString = string;
-console.log(badString);
-
+// Template Literals
 let greeting = `hello, ${string}`;
 console.log(greeting);
+
+let myName = 'Jamie';
+let myCountry = 'New Zealand';
+let greeting = `Hello, my name is ${myName} and I'm from ${myCountry}.`;
+console.log(greeting);
+
+// FOUNDATION PART 2 - EXERCISE 2
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ *
+ * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ */
+
+number = Number(prompt('enter a number'));
+
+function numberChecker() {
+    if (number >= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+birthYear = 1948;
+thisYear = 1965;
+firstName = 'Carlos';
+lastName = 'Stevenson';
+
+greeting = `Hello! My name is ${firstName} ${lastName} and I am ${
+    thisYear - birthYear
+} years old.`;
+console.log(greeting);
+
+// Function Decleration (parameter between the parentheses).
+// Function arguments are the actual values we decide to pass to the function.
+function favoriteAnimal(animal) {
+    console.log(animal + ' is my favorite animal!');
+}
+
+favoriteAnimal('Cat');
