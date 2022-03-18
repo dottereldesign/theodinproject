93fa4ba4128ec7b6733e4d18bc391ba899ab739b
dottereldesign <howemanning@gmail.com>
2022-03-18 23:44:19 +1300

    Checking save state.

--- odinproject.js
+++ odinproject.js
@@ -42,3 +42,16 @@ function favoriteAnimal(animal) {
 }
 
 favoriteAnimal('Cat');
+
+const myText = 'I am a string';
+const newString = myText.replace('string', 'sausage');
+console.log(newString);
+// the replace() string function takes a source string,
+// and a target string and replaces the source string,
+// with the target string, and returns the newly formed string
+
+let myText = 'I am a string';
+let myString = myText.replace('string', 'a peanut.');
+console.log(myString);
+
+// save
