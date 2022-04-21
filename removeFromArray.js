7d199f8475c3e238c28dae8ac2849485121421bc
Jamie Wilson <howemanning@gmail.com>
2022-04-21 15:19:36 +1200

    Created a function that removes a value from an array.

---
+++ removeFromArray.js
@@ -0,0 +1,4 @@
+function removeFromArray(Array, Num) {
+    Num = Array.splice(Num - 1, 1);
+    return Array;
+}
--- script.js
+++ script.js
@@ -14,8 +14,4 @@
 //     }
 // }
 
-const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
 
-for (const cat of cats) {
-    console.log(cat);
-}
