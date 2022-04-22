a4fa729b1e367d6617ddf9b3d58534e2e0115f6d
Jamie Wilson <howemanning@gmail.com>
2022-04-22 18:40:00 +1200

    Created a function that repeats a string x amount of times.

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,9 +1,18 @@
-// Function that returns string.
+// Function that returns string which says, "Hello, World!"
 const helloWorld = function () {
     return 'Hello, World!';
 };
 
-// Function that removes a number from an array.
+// Function that takes a string and then repeats it X amount of times depending on the number which is parsed.
+const repeatString = function (string, num) {
+    if (num >= 0) {
+        return string.repeat(num);
+    } else {
+        return 'ERROR';
+    }
+};
+
+// Function that takes an Array and then removes the number from it which is parsed through.
 function removeFromArray(Array, Num) {
     Num = Array.splice(Num - 1, 1);
     return Array;
