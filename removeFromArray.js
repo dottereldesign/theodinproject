340eff6a70841d41fb9adfd2a8a7e5d432e512e3
Jamie Wilson <howemanning@gmail.com>
2022-04-22 18:35:57 +1200

    Created function that returns a string.

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,3 +1,9 @@
+// Function that returns string.
+const helloWorld = function () {
+    return 'Hello, World!';
+};
+
+// Function that removes a number from an array.
 function removeFromArray(Array, Num) {
     Num = Array.splice(Num - 1, 1);
     return Array;
