fba1b60df13d74926d2269bb7ee9ff907cf67ded
Jamie Wilson <howemanning@gmail.com>
2022-05-14 16:18:37 +1200

    Create function that uses for loop to count up to argument.

---
+++ loops.js
@@ -0,0 +1,53 @@
+for (let step = 0; step < 5; step++) {
+    // Runs 5 times, with values of step 0 through 4.
+    console.log('Walking east one step');
+}
+
+for (let step = 0; step < 5; step++) {
+    let count = 1;
+    console.log(count);
+}
+
+function countSheep(limit) {
+    for (var i = 1; i < limit; i += 1) {
+        console.log(i + ' sheep');
+    }
+}
+
+countSheep(10);
+
+function countNum(maxnum) {
+    for (var i = 1; i < maxnum; i += 1) {
+        console.log(i + ' pie');
+    }
+}
+
+countNum(4);
+
+function countNum(number) {
+    for (var i = 1; i < number; i += 1) {
+        console.log(i + ' mississipi');
+    }
+}
+
+countNum(26);
+
+// create function with number as parameter
+// create for loop
+//
+
+function count(number) {
+    for (let i = 1; i < number; i += 1) {
+        console.log(i + ' cans of beer on the wall');
+    }
+}
+
+count(11);
+
+function count(number) {
+    for (let i = 1; i < number; i += 1) {
+        console.log(i + ' wow');
+    }
+}
+
+count(10);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function count(number) {
+    for (let i = 1; i < number; i += 1) {
+        console.log(i + ' wow');
+    }
+}
+
+count(10);
